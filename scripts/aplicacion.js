8417a6e118ddd3f934dd9b6a80804acaa809cf22
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2020-05-31 21:14:02 -0500

    Tipos de vairbales y de datos

--- scripts/aplicacion.js
+++ scripts/aplicacion.js
@@ -1,1 +1,13 @@
 console.log("Hola mundo");
+// Tipos de variables
+var varible; //Su valor puede variar, es global
+var varLet = 1; //Su valor depende del bloque en el que es definida, es local
+var varconst = 1; //No permite que el valor definido cambie
+// Tipos de datos
+// Ejemplo de algunos tipos de datos
+var numero; //Variable de tipo número, 15, -15, 15.15, etc.
+var cadena; //Varible de tipo texto, "Hola", 'Adios', etc.
+var boleano; //Varible verdadera o falsa.
+var array; //Varible para guardar un arreglo denpendiendo del tipo de dato especificado dentro de <>
+var cualquiera; //Varible de cualquier tipo de dato;
+var objeto; //Varible del tipo objeto
--- scripts/aplicacion.ts
+++ scripts/aplicacion.ts
@@ -1,1 +1,15 @@
 console.log("Hola mundo");
+
+// Tipos de variables
+var varible; //Su valor puede variar, es global
+let varLet = 1; //Su valor depende del bloque en el que es definida, es local
+const varconst = 1; //No permite que el valor definido cambie
+
+// Tipos de datos
+// Ejemplo de algunos tipos de datos
+var numero: Number; //Variable de tipo número, 15, -15, 15.15, etc.
+var cadena: String; //Varible de tipo texto, "Hola", 'Adios', etc.
+var boleano: Boolean; //Varible verdadera o falsa.
+var array: Array<Number>; //Varible para guardar un arreglo denpendiendo del tipo de dato especificado dentro de <>
+var cualquiera: any; //Varible de cualquier tipo de dato;
+var objeto: Object; //Varible del tipo objeto
