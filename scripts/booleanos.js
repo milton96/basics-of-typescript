9f3bd967c878df26cf9b698b44bcf9a4746a6fb2
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2020-06-01 17:47:08 -0500

    Booleanos

---
+++ scripts/booleanos.js
@@ -0,0 +1,11 @@
+var numero1 = 5;
+var numero2 = 10;
+var numero3 = 5;
+var existeNumero = numero1 == numero2;
+console.log(existeNumero);
+existeNumero = numero1 == numero3;
+console.log(existeNumero);
+var verdadero = true;
+var falso = false;
+console.log("Verdadero:", verdadero);
+console.log("Falso:", falso);
---
+++ scripts/booleanos.ts
@@ -0,0 +1,15 @@
+var numero1: number = 5;
+var numero2: number = 10;
+var numero3: number = 5;
+
+var existeNumero: boolean = numero1 == numero2;
+console.log(existeNumero);
+
+existeNumero = numero1 == numero3;
+console.log(existeNumero);
+
+var verdadero: boolean = true;
+var falso: boolean = false;
+
+console.log("Verdadero:", verdadero);
+console.log("Falso:", falso);
