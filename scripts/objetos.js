6181b0ac873ed537f94a595d2a58c442ccbb9b09
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2020-06-01 17:57:36 -0500

    Objetos

---
+++ scripts/objetos.js
@@ -0,0 +1,9 @@
+var persona = {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 25,
+    altura: 1.75,
+    peso: 75
+};
+console.log(persona.nombre, persona.apellido);
+console.log(persona.edad, persona.altura, persona.peso);
---
+++ scripts/objetos.ts
@@ -0,0 +1,10 @@
+var persona = {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 25,
+    altura: 1.75,
+    peso: 75
+}
+
+console.log(persona.nombre, persona.apellido);
+console.log(persona.edad, persona.altura, persona.peso);
