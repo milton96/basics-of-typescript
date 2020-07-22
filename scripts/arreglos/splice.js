59f6cfcf394c73b8193c070d62464fdd0335f1c7
milton bravo <miltonbravo96@gmail.com>
2020-07-22 17:14:56 -0500

    splice

---
+++ scripts/arreglos/splice.js
@@ -0,0 +1,6 @@
+var personas = ["ana", "pedro", "juan", "carlos", "maria"];
+console.log(personas);
+var removed = personas.splice(1, 3);
+console.log("Removidos:");
+console.log(removed);
+console.log(personas);
---
+++ scripts/arreglos/splice.ts
@@ -0,0 +1,10 @@
+var personas: string[] = ["ana", "pedro", "juan", "carlos", "maria"];
+
+console.log(personas);
+
+var removed: string[] = personas.splice(1, 3);
+
+console.log("Removidos:");
+console.log(removed);
+
+console.log(personas);
