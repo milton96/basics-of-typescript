a1d1a4f25bc7da1d6d136f1e76dc286562ff698b
milton bravo <miltonbravo96@gmail.com>
2020-07-22 13:57:57 -0500

    funcion simple

---
+++ scripts/funciones/simple.js
@@ -0,0 +1,7 @@
+function suma() {
+    var numero1 = 1;
+    var numero2 = 2;
+    var total = numero1 + numero2;
+    console.log("numero1 + numero2 =", total);
+}
+suma();
---
+++ scripts/funciones/simple.ts
@@ -0,0 +1,10 @@
+function suma() {
+    var numero1: number = 1;
+    var numero2: number = 2;
+
+    var total: number = numero1 + numero2;
+
+    console.log("numero1 + numero2 =", total);
+}
+
+suma();
