8eb946006eab61b2e5820802e8fef1fdd58050fb
milton bravo <miltonbravo96@gmail.com>
2020-07-21 21:50:24 -0500

    arreglos

--- scripts/arreglos.js
+++ scripts/arreglos.js
@@ -1,1 +1,15 @@
-console.log("arreglos");
+var nombres = ["juan", "pedro", "maría"];
+var numeros = [1, 2, 3, 4];
+var alumnos = [
+    {
+        nombre: "juan",
+        edad: 19
+    },
+    {
+        nombre: "maria",
+        edad: 20
+    }
+];
+console.log(nombres);
+console.log(numeros);
+console.log(alumnos);
--- scripts/arreglos.ts
+++ scripts/arreglos.ts
@@ -1,1 +1,21 @@
-console.log("arreglos");
+interface Alumno {
+    nombre: string;
+    edad: number;
+}
+
+var nombres: Array<string> = ["juan", "pedro", "maría"];
+var numeros: Array<number> = [1, 2, 3, 4];
+var alumnos: Array<Alumno> = [
+    {
+        nombre: "juan",
+        edad: 19
+    },
+    {
+        nombre: "maria",
+        edad: 20
+    }
+]
+
+console.log(nombres);
+console.log(numeros);
+console.log(alumnos);
