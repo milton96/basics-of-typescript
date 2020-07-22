512d40176372d775c8cf3b607588da0497d13e8f
milton bravo <miltonbravo96@gmail.com>
2020-07-21 22:50:29 -0500

    for

---
+++ scripts/ciclos/for.js
@@ -0,0 +1,4 @@
+var nombres = ["maria", "juan", "pedro"];
+for (var index = 0; index < nombres.length; index++) {
+    console.log(nombres[index]);
+}
---
+++ scripts/ciclos/for.ts
@@ -0,0 +1,5 @@
+var nombres: Array<string> = ["maria", "juan", "pedro"];
+
+for (let index = 0; index < nombres.length; index++) {
+    console.log(nombres[index]);    
+}
