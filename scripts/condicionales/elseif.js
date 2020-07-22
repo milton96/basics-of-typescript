1ba2a166e156adb592f0e49ca67435a0ecb6305e
milton bravo <miltonbravo96@gmail.com>
2020-07-21 22:09:52 -0500

    elseif

---
+++ scripts/condicionales/elseif.js
@@ -0,0 +1,10 @@
+var cantidad = 13;
+if (cantidad > 15) {
+    console.log("Hay buen descuento");
+}
+else if (cantidad > 10) {
+    console.log("hay un descuento");
+}
+else {
+    console.log("no hay descuento");
+}
---
+++ scripts/condicionales/elseif.ts
@@ -0,0 +1,9 @@
+var cantidad: number = 13;
+
+if (cantidad > 15) {
+    console.log("Hay buen descuento");
+} else if (cantidad > 10) {
+    console.log("hay un descuento");
+} else {
+    console.log("no hay descuento");
+}
