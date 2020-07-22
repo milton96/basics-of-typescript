6d6389ce21a22590ee4f1bf77faa510af22735b6
milton bravo <miltonbravo96@gmail.com>
2020-07-22 16:40:41 -0500

    funcion return

---
+++ scripts/funciones/return.js
@@ -0,0 +1,5 @@
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+var resultado = multiplicar(2, 6);
+console.log("total:", resultado);
---
+++ scripts/funciones/return.ts
@@ -0,0 +1,7 @@
+function multiplicar(num1: number, num2:number): number {
+    return num1 * num2;
+}
+
+var resultado = multiplicar(2, 6);
+
+console.log("total:", resultado);
