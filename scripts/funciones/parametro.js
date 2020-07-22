f52d1d47c9e252860af51c393586b1064afa348a
milton bravo <miltonbravo96@gmail.com>
2020-07-22 16:34:09 -0500

    funcion con parametros

---
+++ scripts/funciones/parametro.js
@@ -0,0 +1,6 @@
+function sumaConParametro(num1, num2) {
+    var suma = num1 + num2;
+    console.log("La suma es:", suma);
+}
+sumaConParametro(5, 8);
+sumaConParametro(6, 9);
---
+++ scripts/funciones/parametro.ts
@@ -0,0 +1,8 @@
+function sumaConParametro(num1: number, num2: number) {
+    var suma = num1 + num2;
+    console.log("La suma es:", suma);
+}
+
+sumaConParametro(5, 8);
+
+sumaConParametro(6, 9);
