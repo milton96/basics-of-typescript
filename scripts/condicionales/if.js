5621cda0d34e9b0288c210b5e1a93b92780db977
milton bravo <miltonbravo96@gmail.com>
2020-07-21 22:00:12 -0500

    if

---
+++ scripts/condicionales/if.js
@@ -0,0 +1,8 @@
+var cantidad = 15;
+var imprimir = true;
+if (imprimir) {
+    console.info("Estamos imprimiendo...");
+    if (cantidad > 10) {
+        console.log("cantidad alta");
+    }
+}
---
+++ scripts/condicionales/if.ts
@@ -0,0 +1,9 @@
+var cantidad: number = 15;
+var imprimir: boolean = true;
+
+if (imprimir) {
+    console.info("Estamos imprimiendo...");
+    if (cantidad > 10) {
+        console.log("cantidad alta");
+    }
+}
