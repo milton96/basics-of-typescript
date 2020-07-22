f680233c4ee44b4b745bdd1c5bc37709f2c768a3
milton bravo <miltonbravo96@gmail.com>
2020-07-22 17:22:18 -0500

    funciones anonimas

---
+++ scripts/arreglos/funcionesAnonimas.js
@@ -0,0 +1,13 @@
+var lista = [];
+lista.push({ nombre: "maria", apellido: "perez", edad: 20 });
+lista.push({ nombre: "carlos", apellido: "guitierrez", edad: 10 });
+lista.push({ nombre: "pedro", apellido: "martinez", edad: 18 });
+lista.push({ nombre: "juan", apellido: "gonzales", edad: 25 });
+lista.forEach(function (persona) {
+    if (persona.edad >= 18) {
+        console.log("Puedes pasar:", persona.nombre, persona.apellido);
+    }
+    else {
+        console.log("No puedes pasar:", persona.nombre, persona.apellido);
+    }
+});
