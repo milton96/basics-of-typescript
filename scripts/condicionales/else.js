3c92b7462832f05416bc45d785200e97c02ff12d
milton bravo <miltonbravo96@gmail.com>
2020-07-21 22:04:02 -0500

    else

---
+++ scripts/condicionales/else.js
@@ -0,0 +1,7 @@
+var boletos = 25;
+if (boletos > 50) {
+    console.log("hay muchos boletos");
+}
+else {
+    console.log("hay pocos boletos");
+}
---
+++ scripts/condicionales/else.ts
@@ -0,0 +1,7 @@
+var boletos: number = 25;
+
+if (boletos > 50) {
+    console.log("hay muchos boletos");
+} else {
+    console.log("hay pocos boletos");
+}
